253881c5d1861063550654c72e99d755373e165e
Casey Betts <caseyandsusana@gmail.com>
2024-09-23 12:18:52 -0600

    Pulls row deletion forward and streamlines

--- The_Code/Deck_Queries_2.py
+++ The_Code/Deck_Queries_2.py
@@ -10,156 +10,250 @@ from math import floor
 from pathlib import Path
 
 
+class Rivedo():
+    """ Object used to produce the Rivedo shapefile """
 
-def produce_field_mapping(input_layer, number_of_fields_to_move, new_existing_field_mapping):
-    """
-    Creates a field mapping as input for creating a new feature class
-
-    :param input_layer: String, the name of the input feature class or table
-    :param fields_to_move: Integer, the number of fields to move from the end to the beginning
-    :return: String, the name of the new feature class or table with reordered fields
-    """
-    arcpy.env.overwriteOutput = True
-
-    # Get a list of all field names minus the existing ones that will be moved
-    fields = arcpy.ListFields(input_layer)
-    field_names = [field.name for field in fields if field.type not in ['OID', 'Geometry'] + new_existing_field_mapping]
-
-    # Determine the new field order
-    number_of_fields_to_move = min(number_of_fields_to_move, len(field_names))  # Ensure we don't try to move more fields than exist
-    new_field_order = new_existing_field_mapping + field_names[-number_of_fields_to_move:] + field_names[:-number_of_fields_to_move]
-
-    # Create a FieldMappings object
-    field_mappings = arcpy.FieldMappings()
-    field_mappings.addTable(input_layer)
-
-    # Create a new FieldMappings object with fields in the desired order
-    new_field_mappings = arcpy.FieldMappings()
-    for field_name in new_field_order:
-        field_index = field_mappings.findFieldMapIndex(field_name)
-        if field_index != -1:
-            field_map = field_mappings.getFieldMap(field_index)
-            new_field_mappings.addFieldMap(field_map)
-
-    return new_field_mappings
-
-def add_columns_to_feature_class(feature_class, query_input, new_column_input):
-    """
-    Adds the given columns to the given feature layer
-
-    :param feature_class: Feature Class, the feature class to have columns added
-    :param query_input: Dict, specs used by the column functions
-    :param new_column_input: Dict, column parameters to be used as input for the CalculateField funciton
-    """
-
-    for new_column in new_column_input:
+    def __init__(self, active_orders_ufp, hotlist, path):
+        """ Load and initiate data and vars 
         
-        # Open the code block file and save to var
-        with open(new_column_input[new_column]['column_function'], 'r') as data:
-            column_function = data.read()
+            :param active_orders_ufp: Feature Layer, active orders ufp layer
+            :param hotlist: Feature Layer, the hotlist layer
+            :param path: String, path to the folder where the config and output folder is kept
+        """
 
-        # Add column to feature class for new priority
-        field_name = new_column_input[new_column]["field_name"]
-        expression = new_column_input[new_column]["expression"]
-        code_block = "query_input =" + str(query_input) + "\n" + column_function
-        field_type = new_column_input[new_column]["field_type"]
-        arcpy.management.CalculateField(feature_class, field_name, expression, "PYTHON3", code_block, field_type)
+        self.config_path = Path( path + r"\The_Code\Sensitive_Parameters.json")
 
-def get_layer_by_name(layer_name, map):
-    """
-    Returns the first layer in the TOC of the given name
+        # Load .json config file
+        with open(self.config_path, 'r', errors="ignore") as input:
+            self.config = json.load(input)
 
-    :param layer_name: String, the name of the layer to be returned
-    """
+        # Define the paths to the parameters and the outputs
+        self.temp_loc = arcpy.env.workspace
+        self.temp_name = "Rivedo_temp"
+        self.temp_feature_class = self.temp_loc + "\\" + self.temp_name
+        self.output_loc = path + r"\Shapefile"
+        self.output_name = "Rivedo_orders"
+        self.staging_location = os.path.join(path, "Shapefile_Staging")
+        self.active_orders_ufp = active_orders_ufp
+        self.hotlist = hotlist
 
-    # Find the layer
-    for layer in map.listLayers():
-        if layer.name == layer_name:
-            return layer
-    else:
-        raise Exception(f"Source layer '{layer_name}' not found in the TOC.")
+        # Produce active customer list
+        self.active_cust_info = self.produce_cust_info()
 
-def select_and_delete_rows(layer, rivedo_where_clause, excluded_priorities):
-    """ 
-    Selects and deletes rows based on a given set of criteria
+        # Get the active map document and data frame
+        self.project = arcpy.mp.ArcGISProject("CURRENT")
+        self.map = self.project.activeMap
+
+        # Create hotlist soli list and where clause string
+        hotlist_solis = self.get_field_values(self.hotlist, "soli")
+
+        # Create vars for the rivedo priority and middle digit from the configs 
+        rivedo_priority = self.config["new_column_input"]["Rivedo_Priority"]["field_name"]
+        middle_digit = self.config["new_column_input"]["Middle_Digit"]["field_name"]
+
+        # Create a list of where clauses to select rows to delete
+        self.where_clauses = ["tasking_priority" + f" = {rivedo_priority} And {middle_digit} <> 'Low' And {middle_digit} <> 'High' ", 
+                              "tasking_priority" + " IN " + "(" + ",".join(str(num) for num in self.config["excluded_priorities"]) + ")", 
+                              "ge01 = 0 And wv01 = 0 And wv02 = 0 And wv03 = 0",
+                              "sap_customer_identifier" + " IN " + "(" + ",".join("'"+str(num)+"'" for num in self.config["customer_info"]["idi_customers"]) + ")", 
+                              "external_id" + " IN " + "(" + ",".join("'"+str(num)+"'" for num in hotlist_solis) + ")"]
+
+        # Call functions
+        self.run_workflow()
+
+
+    def add_columns_to_feature_class(self, active_customer_info):
+        """
+        Adds the given columns to the given feature layer
+
+        """
+
+        for new_column in self.config["new_column_input"]:
+            
+            # Create column specific vars
+            field_name = self.config["new_column_input"][new_column]["field_name"]
+            expression = self.config["new_column_input"][new_column]["expression"]
+            config_reqs = self.config["new_column_input"][new_column]["config_reqs"]
+            field_type = self.config["new_column_input"][new_column]["field_type"]
+
+            # Create code block string
+            with open(self.config["new_column_input"][new_column]['column_function'], 'r') as data:
+                column_function = data.read()
+            
+            if config_reqs == "active_customer_info":
+                code_block = "active_customer_info = " + str(active_customer_info) + "\n"
+            else:
+                code_block = config_reqs + " = " + str(self.config[config_reqs]) + "\n"
+
+            code_block = code_block + column_function
+
+            # Add column to feature class
+            arcpy.management.CalculateField(self.temp_feature_class, field_name, expression, "PYTHON3", code_block, field_type)
+
+    def produce_field_mapping(self):
+        """
+        Creates a field mapping as input for creating a new feature class
+
+        :param input_layer: String, the name of the input feature class or table
+        :param fields_to_move: Integer, the number of fields to move from the end to the beginning
+        :return: String, the name of the new feature class or table with reordered fields
+        """
+        arcpy.env.overwriteOutput = True
+
+        # Get a list of all field names minus the existing ones that will be moved
+        fields = arcpy.ListFields(self.temp_name)
+        field_names = [field.name for field in fields if field.type not in ['OID', 'Geometry'] + self.config["new_existing_field_mapping"]]
+
+        # Determine the new field order
+        number_of_fields_to_move = min(len(self.config["new_column_input"]), len(field_names))  # Ensure we don't try to move more fields than exist
+        new_field_order = self.config["new_existing_field_mapping"] + field_names[-number_of_fields_to_move:] + field_names[:-number_of_fields_to_move]
+
+        # Create a FieldMappings object
+        field_mappings = arcpy.FieldMappings()
+        field_mappings.addTable(self.temp_name)
+
+        # Create a new FieldMappings object with fields in the desired order
+        new_field_mappings = arcpy.FieldMappings()
+        for field_name in new_field_order:
+            field_index = field_mappings.findFieldMapIndex(field_name)
+            if field_index != -1:
+                field_map = field_mappings.getFieldMap(field_index)
+                new_field_mappings.addFieldMap(field_map)
+
+        return new_field_mappings
     
-    :param layer: Feature Layer, the layer to be edited
-    :param excluded_priorities, List, a list of priorities to exclude
-    """
+    def customer_name(self, cust, customer_info):
+        """ 
+        Returns the customer name given the customer number and the dict of all customers
+        
+        :param cust: Int, customer id
+        :param customer_info: Dict, dictionary of customer ids and names
+        """
 
-    # Select the desired rows
-    arcpy.management.SelectLayerByAttribute(layer, "NEW_SELECTION", rivedo_where_clause)
-    arcpy.management.SelectLayerByAttribute(layer, "SWITCH_SELECTION")
-    exclusion_string = "(" + ",".join(str(num) for num in excluded_priorities) + ")"
-    exlusion_where_clause = "tasking_priority"[:10] + " IN " + exclusion_string
-    arcpy.management.SelectLayerByAttribute(layer, "ADD_TO_SELECTION", exlusion_where_clause)
-    arcpy.management.DeleteFeatures(layer)
+        for item in customer_info:
+            if cust in customer_info[item]:
+                return customer_info[item][cust]
+        
+        return "--"
+    
+    def get_layer_by_name(self, layer_name, map):
+        """
+        Returns the first layer in the TOC of the given name
 
-def delete_current_files(file_location, file_names):
-    """ Deletes out the current files in the folder """
+        :param layer_name: String, the name of the layer to be returned
+        """
 
-    for file in file_names:
-        file_path = os.path.join(file_location,  file)
-        if os.path.exists(file_path):
-            os.remove(file_path)
-            arcpy.AddMessage(f"Deleted: {file}")
+        # Find the layer
+        for layer in map.listLayers():
+            if layer.name == layer_name:
+                return layer
+        else:
+            raise Exception(f"Source layer '{layer_name}' not found in the TOC.")
+        
+    def delete_current_files(self):
+        """ 
+        Deletes out the files of the given name in the given folder
+        
+        :param file_location: String, path to the target folder
+        :param file_names: String, name of file(s) to be deleted
+        """
 
-def move_new_files(staging_location, output_location):
-    """ Moves the newly generated files to the output folder """
+        for file in self.output_name:
+            file_path = os.path.join(self.output_loc,  file)
+            if os.path.exists(file_path):
+                os.remove(file_path)
+                arcpy.AddMessage(f"Deleted: {file}")
 
-    for file in os.listdir(staging_location):
-        source_path = os.path.join(staging_location, file)
-        dest_path = os.path.join(output_location, file)
-        shutil.move(source_path, dest_path)
-        arcpy.AddMessage(f"Moved: {file}")
+    def move_files(self):
+        """ 
+        Moves all files in a given folder to the given output folder
+        
+        :param initial_location: String, path to the 'move from' folder
+        :param final_location: String, path to the 'move to' folder
+        """
 
-def run(active_orders_ufp, path):
-    """ This is the function called from the arc tool """
+        for file in os.listdir(self.staging_location):
+            source_path = os.path.join(self.staging_location, file)
+            dest_path = os.path.join(self.output_loc, file)
+            shutil.move(source_path, dest_path)
+            arcpy.AddMessage(f"Moved: {file}")
+         
+    def produce_cust_info(self):
+        """
+        Returns a dictionary of only active customer ids and names
 
-    # Define the paths to the parameters and the outputs
-    parameters_path = Path( path + r"\The_Code\Sensitive_Parameters.json")
-    temp_loc = arcpy.env.workspace
-    temp_name = "Rivedo_temp"
-    temp_feature_class = temp_loc + "\\" + temp_name
-    output_loc = path + r"\Shapefile"
-    output_name = "Rivedo_orders"
-    staging_location = os.path.join(path, "Shapefile_Staging")
+        :param active_orders: Feature Layer, active orders ufp layer
+        :param customer_info: Dict, all customer info from the config file
+        """
 
-    # Load .json file with parameters
-    with open(parameters_path, 'r', errors="ignore") as input:
-        config = json.load(input)
+        with arcpy.da.SearchCursor(self.active_orders_ufp, ["sap_customer_identifier"]) as cursor:
+            active_customers = sorted({row[0] for row in cursor})
 
-    # Generate the new field mapping
-    new_field_mapping = produce_field_mapping(temp_name, len(config["new_column_input"]), config["new_existing_field_mapping"])
+        cust_dict = dict()
 
-    # Set the string for the where clause that determines which rows to remove   
-    rivedo_priority = config["new_column_input"]["Rivedo_Priority"]["field_name"]
-    middle_digit = config["new_column_input"]["Middle_Digit"]["field_name"]
-    rivedo_where_clause = "tasking_priority"[:10] + f"<> {rivedo_priority} Or {middle_digit} = 'Low' Or {middle_digit} = 'High' "
+        for id in active_customers:
+            cust_dict[str(id)] = self.customer_name(id, self.config["customer_info"])
 
-    # Get the active map document and data frame
-    project = arcpy.mp.ArcGISProject("CURRENT")
-    map = project.activeMap
+        return cust_dict
 
-    # Create a temporary feature class of the orders layer
-    arcpy.conversion.ExportFeatures(active_orders_ufp, temp_feature_class)
+    def get_field_values(self, layer, field):
+        """ 
+        Returns a list of values from a given field in a given layer
+        
+        :param layer: Feature Layer, the layer with the values to stor in a list
+        :param field: String, the name of the field to gather values from
+        """
 
-    # Add columns to the temp feature class
-    add_columns_to_feature_class(temp_feature_class, config["query_inputs"], config["new_column_input"])
+        # initialize list
+        values = []
 
-    # Add the feature layer to the map
-    map.addDataFromPath(temp_feature_class)
+        # Append all values into the list
+        with arcpy.da.SearchCursor(layer, [field]) as cursor:
+            for row in cursor:
+                values.append(row[0])
 
-    # Create a new feature class with the reordered fields
-    arcpy.conversion.ExportFeatures(temp_name, staging_location + "\\" + output_name, field_mapping = new_field_mapping)
+        return values
 
-    # Delete the existing output files
-    delete_current_files(output_loc, output_name)
+    def select_and_delete_rows(self, layer):
+        """ Select and delete rows from the given layer given a list of where clauses
 
-    # Move the produced files to the final output location
-    move_new_files(staging_location, output_loc)
+        :param layer, Feature Layer, the layer to select and delete rows from
+        """
 
-    # Remove the temp layer from the map
-    map.removeLayer(get_layer_by_name(temp_name, map))
+        # Select rows to delete
+        for clause in self.where_clauses:
+            arcpy.management.SelectLayerByAttribute(layer, "ADD_TO_SELECTION", clause)
 
-    select_and_delete_rows(get_layer_by_name(output_name, map), rivedo_where_clause, config["excluded_priorities"])
+        # Delete selected rows
+        arcpy.management.DeleteFeatures(layer)
+
+    def run_workflow(self):
+        """ This function calls all functions in the needed order to produce final output """
+
+        # Create a temporary feature class of the orders layer
+        arcpy.conversion.ExportFeatures(self.active_orders_ufp, self.temp_feature_class)
+
+        # Add columns to the temp feature class
+        self.add_columns_to_feature_class(self.active_cust_info)
+
+        # Generate the new field mapping
+        new_field_mapping = self.produce_field_mapping()
+
+        # Add the feature calss to the map as a new feature layer
+        self.map.addDataFromPath(self.temp_feature_class)
+
+        # Select and Delete rows from the temp featuer layer
+        self.select_and_delete_rows(self.temp_name) 
+
+        # Create a new feature class with the reordered fields
+        arcpy.conversion.ExportFeatures(self.temp_name, self.staging_location + "\\" + self.output_name, field_mapping = new_field_mapping)
+
+        # Delete the existing output files
+        self.delete_current_files()
+
+        # Move the produced files to the final output location
+        self.move_files()
+
+        # Remove the temp layer from the map
+        self.map.removeLayer(self.get_layer_by_name(self.temp_name, self.map))
+
